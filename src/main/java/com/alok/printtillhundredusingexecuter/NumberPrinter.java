7b404fdb8b70c8facd66538f84c5b45720c9e2f4
galok8749 <galok8749@gmailcom>
2023-03-26 23:43:49 +0530

    print using executers

---
+++ src/main/java/com/alok/printtillhundredusingexecuter/NumberPrinter.java
@@ -0,0 +1,12 @@
+package com.alok.printtillhundredusingexecuter;
+
+public class NumberPrinter implements Runnable {
+    private int numberToPrint;
+    NumberPrinter(int numberToPrint) {
+        this.numberToPrint = numberToPrint;
+    }
+    @Override
+    public void run() {
+        System.out.println(numberToPrint + " " + Thread.currentThread().getName());
+    }
+}
