a9122f1a18256b9338073aa379773364b42423e9
galok8749 <galok8749@gmailcom>
2023-03-26 23:01:44 +0530

    print 1 to 100

---
+++ src/main/java/com/alok/printtillhundred/Main.java
@@ -0,0 +1,12 @@
+package com.alok.printtillhundred;
+
+public class Main {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 100; i++) {
+            NumberPrinter numberPrinter = new NumberPrinter(i);
+            Thread t = new Thread(numberPrinter);
+            t.start();
+        }
+    }
+}
